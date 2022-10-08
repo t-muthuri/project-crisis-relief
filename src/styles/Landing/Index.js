5e68553cbf2ae013d95f8ffd3090f86fe7d4b5d0
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-10-08 10:14:45 +0300

    Styling the Landing page

--- src/styles/Landing/Index.js
+++ src/styles/Landing/Index.js
@@ -9,4 +9,22 @@ export const LandingContainer = styled(Box)(() => ({
     height: "100%",
     padding: "0px 0px",
     background: Colors.light_grey,
+    [theme.breakpoints.down("sm")]:{
+        flexDirection: "column",
+        //sets how flex items are placed in a flex container defining the main axis and the direction
+        //column - the flex container's main axis is the same as the block-axis
+
+        alignItems: "center"
+    }
+}))
+
+export const LandingContent = styled(Box)(() => ({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    maxWidth: 420,
+    //sets the maximum width of an element
+    //overrrides the used value and the width properties. prevents them from becoming larger than the value specified.
+
+    padding: "30px"
 }))
